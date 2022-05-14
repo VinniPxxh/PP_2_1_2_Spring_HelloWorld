0b1e176c125046fb561679fa0270b0e7d6eab634
VinniPxxh <kuznetsove2001@mail.ru>
2022-05-14 15:01:24 +0300

    Final commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,16 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+
+        boolean comparisonSimpleBean = bean == bean2;
+        boolean comparisonCatBean = catBean == catBean2;
+        System.out.println(comparisonSimpleBean + "\n" + comparisonCatBean);
+
         System.out.println(bean.getMessage());
+        System.out.println((catBean.getMessage()));
     }
 }
